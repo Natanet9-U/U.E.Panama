761ebf0606d9886ca247c9b9d5e454dbbb40a8af
Aldirapc <aldirapigui2005@gmail.com>
2026-04-01 14:41:44 -0400

    Add files via upload
    feat: add login component

---
+++ frontend/login/Login.jsx
@@ -0,0 +1,49 @@
+import "./Login.css";
+ 
+function Login() {
+  return (
+    <div className="container">
+ 
+      <div className="card">
+ 
+        <h1 className="title">SISTEMA ACADÉMICO</h1>
+ 
+        <p className="subtitle">
+          INICIA SESIÓN PARA CONTINUAR
+        </p>
+ 
+        <form className="form">
+ 
+          <div className="input-group">
+            <label>CORREO ELECTRONICO</label>
+            <input
+              type="email"
+              placeholder="Ingrese su correo"
+            />
+          </div>
+ 
+          <div className="input-group">
+            <label>CONTRASEÑA</label>
+            <input
+              type="password"
+              placeholder="Ingrese su contraseña"
+            />
+          </div>
+ 
+          <a href="#" className="link">
+            ¿OLVIDO SU CONTRASEÑA?
+          </a>
+ 
+          <button>
+            INICIAR SESIÓN
+          </button>
+ 
+        </form>
+ 
+      </div>
+ 
+    </div>
+  );
+}
+ 
+export default Login;
